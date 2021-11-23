5ab06b643db6dde3d1723785af2a34648a1168c0
Angus Ryer <angus@ryer.ca>
2021-11-23 09:42:56 -0500

    updated for clarity

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,42 +8,44 @@ const Home: NextPage = () => {
 
 	useEffect(() => {
 		if (endpoint !== "") {
-			fetch(`/api/${endpoint}`).then((res) => setEndpointResponse(res.url));
+			fetch(`/api${endpoint}`).then((res) => {
+				setEndpointResponse(res.status.toString());
+			});
 		}
 	}, [endpoint]);
 
 	return (
 		<div className={styles.container}>
-			<h3>Response from {endpoint}:</h3>
-			<h2>{endpointResponse}</h2>
+			<h3>Endpoint clicked: {endpoint}:</h3>
+			<h2>Response status: {endpointResponse}</h2>
 			<div style={{ display: "flex", flexDirection: "column" }}>
 				<button
 					style={{ margin: "5px", width: "260px", textAlign: "left" }}
 					onClick={() => setEndpoint("/")}
 				>
-					root /
+					/index
 				</button>
 				<button
 					style={{ margin: "5px", width: "260px", textAlign: "left" }}
-					onClick={() => setEndpoint("mock1/")}
+					onClick={() => setEndpoint("/mock1")}
 				>
 					mock1/index
 				</button>
 				<button
 					style={{ margin: "5px", width: "260px", textAlign: "left" }}
-					onClick={() => setEndpoint("mock1/mock1-endpoint")}
+					onClick={() => setEndpoint("/mock1/mock1-endpoint")}
 				>
 					mock1/mock1-endpoint
 				</button>
 				<button
 					style={{ margin: "5px", width: "260px", textAlign: "left" }}
-					onClick={() => setEndpoint("mock2/")}
+					onClick={() => setEndpoint("/mock2")}
 				>
 					mock2/index
 				</button>
 				<button
 					style={{ margin: "5px", width: "260px", textAlign: "left" }}
-					onClick={() => setEndpoint("mock2/mock2-endpoint")}
+					onClick={() => setEndpoint("/mock2/mock2-endpoint")}
 				>
 					mock2/mock2-endpoint
 				</button>
