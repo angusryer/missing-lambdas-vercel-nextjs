966b703361a735b30d1d935af1cc296ff8dcf057
Angus Ryer <angus@ryer.ca>
2021-11-23 10:33:32 -0500

    updated example

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import styles from "../styles/Home.module.css";
 const Home: NextPage = () => {
 	const [endpoint, setEndpoint] = useState("");
 	const [endpointResponse, setEndpointResponse] = useState("");
+	const [dynamicEndpoint, setDynamicEndpoint] = useState("");
 
 	useEffect(() => {
 		if (endpoint !== "") {
@@ -19,6 +20,7 @@ const Home: NextPage = () => {
 			<h3>Endpoint clicked: {endpoint}:</h3>
 			<h2>Response status: {endpointResponse}</h2>
 			<div style={{ display: "flex", flexDirection: "column" }}>
+				<h4>Static endpoints:</h4>
 				<button
 					style={{ margin: "5px", width: "260px", textAlign: "left" }}
 					onClick={() => setEndpoint("/")}
@@ -49,6 +51,45 @@ const Home: NextPage = () => {
 				>
 					mock2/mock2-endpoint
 				</button>
+				<hr />
+				<h4>Dynamic endpoints:</h4>
+				<button
+					style={{ margin: "5px", width: "260px", textAlign: "left" }}
+					onClick={() => setEndpoint("/mock1/001")}
+				>
+					mock1/001
+				</button>
+				<button
+					style={{ margin: "5px", width: "260px", textAlign: "left" }}
+					onClick={() => setEndpoint("/mock1/002")}
+				>
+					mock1/002
+				</button>
+				<button
+					style={{ margin: "5px", width: "260px", textAlign: "left" }}
+					onClick={() => setEndpoint("/mock1/003")}
+				>
+					mock1/003
+				</button>
+				<button
+					style={{ margin: "5px", width: "260px", textAlign: "left" }}
+					onClick={() => setEndpoint("/mock2/001")}
+				>
+					mock2/001
+				</button>
+				<button
+					style={{ margin: "5px", width: "260px", textAlign: "left" }}
+					onClick={() => setEndpoint("/mock2/002")}
+				>
+					mock2/002
+				</button>
+				<button
+					style={{ margin: "5px", width: "260px", textAlign: "left" }}
+					onClick={() => setEndpoint("/mock2/003")}
+				>
+					mock2/003
+				</button>
+				<hr />
 			</div>
 		</div>
 	);
